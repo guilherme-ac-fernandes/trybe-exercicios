67ab853db670dd2b8688b52bd4b10899dbb98e7a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-02-04 00:56:52 -0300

    Realizando exercícios bloco 4 dia 3 - Finalizados 1 e 2 + Em processo 3 e 4

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-3.js
@@ -0,0 +1,10 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let bigWord = 0;
+let smallWord = 0;
+
+// Informar maior e menor palavras no Array
+
+
+
+console.log(bigWord);
+console.log(smallWord);
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-4.js
@@ -0,0 +1,8 @@
+let number = 50;
+let dividor = 1;
+
+// Encontrar números primos entre um intervalo (0 e 50)
+
+for (let i = 0; i < number; i+=1) {
+   
+}
