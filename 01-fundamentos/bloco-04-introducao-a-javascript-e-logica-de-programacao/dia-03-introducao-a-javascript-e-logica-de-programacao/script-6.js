d8f3e347a3dace05043551ca2e41e4b1956a3d80
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-02-04 22:52:27 -0300

    Realizando exercícios até o oitavo e conclusão do README

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-6.js
@@ -0,0 +1,14 @@
+let n = 5;
+let symbol = "#";
+let noSymbol = " ";
+let inputLine = "";
+
+
+// Imprime uma pirâmide de tamanho N a esquerda
+
+
+for(let i = 0; i <= n; i +=1){
+    console.log(inputLine);
+    inputLine = inputLine + symbol; 
+}
+
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-8.js
@@ -0,0 +1,10 @@
+let n = 5;
+let symbol = "#";
+let noSymbol = " ";
+let inputLine = "";
+let inputPosition = n;
+
+
+// Imprime uma pirâmide de tamanho N centralizada
+
+
