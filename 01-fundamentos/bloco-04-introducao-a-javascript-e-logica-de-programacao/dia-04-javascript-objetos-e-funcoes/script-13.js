bca209dcd46cbd9505d46544d526fbd2ff2d9f69
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-02-07 18:41:34 -0300

    Resolução do exercício 13

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-13.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-13.js
@@ -7,16 +7,17 @@ function repeticao (array) {
     let contador = 0;
     let numero = null;
     for (let i = 0; i < array.length; i++) {
+        contador = 0;
         for (let j = 0; j < array.length; j++) {
             if (array[j] == array[i]) {
                 contador +=1;
             }
         }
         if (contador > maiorRepeticao){
-            maiorRepeticao = maiorRepeticao;
+            maiorRepeticao = contador;
             numero = array[i];
         }
-        contador = 0;
+        
     }
     console.log("O número com a maior repetiçõa é o " + numero + " que apareceu " + maiorRepeticao + " vezes.");
 }
