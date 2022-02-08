dc13b4aab3afc769af1a2c45b02445efb45f6986
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-02-08 10:37:49 -0300

    Realização dos exercícios 17, 19 e 20

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-17.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-17.js
@@ -1,3 +1,17 @@
+let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+
+// Criar uma função que encontre os números pares dentro do vetor/array
 
 
-// 
+function arrayOfNumbers(array) {
+  array = array.flat();
+  let arrayEven = [];
+  for (let i = 1; i <= array.length; i+=1) {
+    if(array[i] % 2 == 0){
+      arrayEven.push(array[i]);
+    }
+  }
+    console.log(arrayEven);
+}
+
+arrayOfNumbers(vector);
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-18.js
@@ -0,0 +1,41 @@
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+
+
+// Criar um objeto com a quantidade de itens na cesta
+
+
+function countBasket (array) {
+	let basketSummary = {};
+
+
+	for (let i = 0; i < array.length; i++) {
+		
+		for (let j = 0; j < array.length; j++) {
+			if (array[i] === array[j]){
+				basketSummary["Melancia"] = 1;
+			}
+		}
+	}
+	console.log(basketSummary)
+}
+
+countBasket(basket);
