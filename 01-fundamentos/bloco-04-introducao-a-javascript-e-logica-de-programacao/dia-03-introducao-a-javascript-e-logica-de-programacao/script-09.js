c07892cb483613c4b5916d77a5590cab4071bf21
Guilherme Fernandes <97487898+guilherme-ac-fernandes@users.noreply.github.com>
2022-02-10 12:16:29 -0300

    Update script-09.js

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-09.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-09.js
@@ -4,6 +4,9 @@ let noSymbol = " ";
 
 // Imprime uma pirâmide de tamanho N centralizada com espaços internos
 
+// Forma de estruturação da pirâmide central proveniente do course da Trybe
+// source: https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-logica-de-programacao-e-algoritmos/solutions/83bb1cd0-be4a-40cf-a9c2-22d71f700144/gabarito-dos-exercicios/dbef6a68-2974-4129-9de4-9031d8d30b94?use_case=calendar
+
 
 let middle = (n + 1)/2;
 let controlLeft = middle;
