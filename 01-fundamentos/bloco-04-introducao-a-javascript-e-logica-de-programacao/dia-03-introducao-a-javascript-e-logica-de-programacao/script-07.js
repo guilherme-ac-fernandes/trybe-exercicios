923cf992984b9b48321fc8190bd5520354d46bfa
Guilherme Fernandes <97487898+guilherme-ac-fernandes@users.noreply.github.com>
2022-02-10 12:13:37 -0300

    Update script-07.js

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-07.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-introducao-a-javascript-e-logica-de-programacao/script-07.js
@@ -7,6 +7,8 @@ let inputPosition = n;
 
 // Imprime uma pirâmide de tamanho N a direita
 
+// Forma de estruturação da pirâmide direita retirada do course da Trybe
+// source: https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-logica-de-programacao-e-algoritmos/solutions/83bb1cd0-be4a-40cf-a9c2-22d71f700144/gabarito-dos-exercicios/dbef6a68-2974-4129-9de4-9031d8d30b94?use_case=calendar
 
 for(let i = 0; i < n; i +=1){
     for (let j = 0; j <= n; j+=1) {
