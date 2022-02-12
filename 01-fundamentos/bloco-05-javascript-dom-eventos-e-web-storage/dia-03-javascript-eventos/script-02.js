65bc0b70b29e32a4ae95f556cd92f8c5c3e9efd6
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-02-11 22:39:21 -0300

    Resolução da responsividade do botão com ajuda do gabarito da Trybe

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script-02.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script-02.js
@@ -1,55 +1,75 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
   
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
   
-      weekDaysList.appendChild(dayListItem);
-    };
+    weekDaysList.appendChild(dayListItem);
   };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+// Escreva seu código abaixo.
 
-  // Exercício 1
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+// Exercício 1
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let individualDayPosition = document.getElementById("days");
+for (let i = 0; i < dezDaysList.length; i += 1) {
+  let individualDay = dezDaysList[i];
+  let individualDayElement = document.createElement("li");
+  individualDayElement.className = "day";
+  individualDayElement.innerText = individualDay;
+  individualDayPosition.appendChild(individualDayElement);
+  if (individualDay === 25){
+    individualDayElement.className = "day holiday friday";
+  } else if (individualDay === 24 || individualDay === 31){
+    individualDayElement.className = "day holiday";
+  } else if (individualDay === 4 || individualDay === 11 || individualDay === 18) {
+    individualDayElement.className = "day friday";
+  }
+};
 
-  let individualDayPosition = document.getElementById("days");
+// Exercício 2
+function createButton(stringId, stringText) {
+  let buttonPosition = document.querySelector(".buttons-container");
+  let button = document.createElement("button");
+  button.id = stringId;
+  button.innerText = stringText;
+  buttonPosition.appendChild(button);
+};
+createButton("btn-holiday", "Feriado");
 
-  for (let i = 0; i < dezDaysList.length; i += 1) {
-      let individualDay = dezDaysList[i];
-      let individualDayElement = document.createElement("li");
-      individualDayElement.className = "day";
-      individualDayElement.innerText = individualDay;
-      individualDayPosition.appendChild(individualDayElement);
-      if (individualDay === 25){
-        individualDayElement.className = "day holiday friday";
-      } else if (individualDay === 24 || individualDay === 31){
-        individualDayElement.className = "day holiday";
-      } else if (individualDay === 4 || individualDay === 11 || individualDay === 18) {
-        individualDayElement.className = "day friday";
+// Exercício 3
+// Função inversa de colocar e remover destaque proveniente do course da Trybe
+// source: https://app.betrybe.com/course/fundamentals/javascript-dom-eventos-e-web-storage/javascript-eventos-gabarito/solutions/700128ff-5150-4320-9b33-875c7e6d5a2e/gabarito-dos-exercicios/b1d700e5-2b66-48c3-8af0-310cb17eacda?use_case=calendar
+function holidayChange () {
+  let holidayButton = document.getElementById("btn-holiday");
+  let holiday = document.querySelectorAll(".holiday");
+  let backgroundColor = "rgb(238,238,238)";
+  let newBackgroundColorChange = "white";
+  holidayButton.addEventListener("click", function () {
+    for (let i = 0; i < holiday.length; i += 1) {
+      let position = holiday[i];
+      if (position.style.background === newBackgroundColorChange) {
+        position.style.background = backgroundColor;
+      } else {
+        position.style.background = newBackgroundColorChange;
       }
     }
+  })
+};
 
-    // Exercício 2
-    function createButton(stringId, stringText) {
-      let buttonPosition = document.querySelector(".buttons-container");
-      let button = document.createElement("button");
-      button.id = stringId;
-      button.innerText = stringText;
-      buttonPosition.appendChild(button);
-    };
-    createButton("btn-holiday", "Feriado");
+holidayChange();
 
-    // Exercício 3
+// Exercício 4
+// Já tinha criado a função criar botão, apenas alterei as strings de entrada
+createButton("btn-friday", "Sexta-Feira");
 
-    let holidayButton = document.getElementById("btn-holiday");
-    holidayButton.addEventListener("click", function (event) {
-      event.innerText = "Gui"; 
-    });
+// Exercício 5
+
 
 
