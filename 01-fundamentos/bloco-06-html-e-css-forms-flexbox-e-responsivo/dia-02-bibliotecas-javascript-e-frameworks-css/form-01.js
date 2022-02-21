8576027cae4824ddc39d77ff28cd54072df78ec7
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-02-21 20:31:51 -0300

    Resolução do exercício completa

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/form-01.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/form-01.js
@@ -5,69 +5,60 @@ const emailInput = document.getElementById('input-email');
 const textInput = document.getElementById('input-texto-longo');
 const placeInput = document.querySelectorAll('.opcao');
 
-function countCharacter(string) {
-  let count = 0;
-  for (let j = 0; j < string.length; j += 1) {
-    count += 1;
-  }
-  return count;
-}
+// Pegar o dia mínimo para o calendário, função presente no link abaixo
+// source: https://www.codegrepper.com/code-examples/javascript/get+current+date+javascript+yyyy-mm-dd
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //
+let yyyy = today.getFullYear();
+today = yyyy +'-'+ mm +'-'+ dd;
 
-function countPlace() {
-  const opcao1 = document.getElementById('opcao1');
-  const opcao2 = document.getElementById('opcao2');
-  const opcao3 = document.getElementById('opcao3');
-  const opcao4 = document.getElementById('opcao4');
-  let count = 0;
-  if(opcao1.checked === true){
-    count += 1;
-  }
-  if(opcao2.checked === true){
-    count += 1;
-  }
-  if(opcao3.checked === true){
-    count += 1;
-  }
-  if(opcao4.checked === true){
-    count += 1;
-  }
-  console.log(count);
-  return count
-}
+// Incluindo Pikaday na input da data
+// source: https://github.com/Pikaday/Pikaday
+const dateInput = document.getElementById('data');
+let picker = new Pikaday({ 
+  field: document.getElementById('data'),
+  minDate: new Date(today),
+
+});
+
+// Incluindo biblioteca JavaScript de validação
+// source: https://github.com/horprogs/Just-validate
+const validate = new window.JustValidate('#form');
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#input-nome', [
+    {
+      rule: 'minLength',
+      value: 10,
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+    }
+  ])
+  .addField('#input-email', [
+    {
+      rule: 'maxLength',
+      value: 50,
+    }
+  ])
+  .addField('#input-texto-longo', [
+    {
+      rule: 'maxLength',
+      value: 500,
+    }
+  ]);
 
 window.onload = () => {
-  // submitButton.addEventListener('click', (event) => {
-  //   event.preventDefault();
-  // });
   agreedImage.addEventListener('click', () => {
     if (agreedImage.checked === true) {
-      const imagePosition = document.getElementById('imageFieldset');
+      const imagePosition = document.getElementById('div-img');
       const image = document.createElement('input');
       image.type = 'file';
       image.class = 'agreedImage'
       imagePosition.appendChild(image);
     }
   });
-  submitButton.addEventListener('click', () => {
-    let countName = countCharacter(nameInput.value);
-    let countEmail = countCharacter(emailInput.value);
-    let countText = countCharacter(textInput.value);
-    if (countName < 10){
-      console.log('1')
-      return alert('Dados Inválidos');
-    } else if (countName > 50 ){
-      console.log('2')
-      return alert('Dados Inválidos');
-    } else if (countEmail > 50 ){
-      console.log('3')
-      return alert('Dados Inválidos');
-    } else if (countText > 500){
-      console.log('4')
-      return alert('Dados Inválidos');
-    } else if (countPlace() !== 1){
-      console.log('5')
-      return alert('Dados Inválidos');
-    }
-    return alert('Dados enviados com sucesso!');
-  })
 }
