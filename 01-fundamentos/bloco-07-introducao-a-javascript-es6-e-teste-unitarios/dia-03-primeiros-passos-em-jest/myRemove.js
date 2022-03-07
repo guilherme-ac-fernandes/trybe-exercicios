e6cd27ecef076ed761d0e5a50b84da122e597005
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-07 13:54:32 -0300

    Resolução do primeiro exercício e criando do arquivo do segundo exercício

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-primeiros-passos-em-jest/myRemove.js
@@ -0,0 +1,13 @@
+// As funções que serão submetidas aos testes
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// Os teste das funções
+// implemente seus testes aqui
---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-teste-unitarios/dia-03-primeiros-passos-em-jest/sum.js
@@ -0,0 +1,10 @@
+// Arquivos com as funções que serão submetidas aos testes
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+module.exports = sum;
