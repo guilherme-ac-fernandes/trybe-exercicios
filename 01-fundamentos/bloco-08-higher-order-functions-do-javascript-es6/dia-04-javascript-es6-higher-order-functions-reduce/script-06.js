e7704de1b2a7e824ce2f798a8125043bc3e5173d
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-13 20:00:17 -0300

    Resolução do sexto exercício

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/script-06.js
@@ -0,0 +1,14 @@
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+// Criar objeto juntando informações de dois array (nome e média das notas)
+function studentAverage(array1, array2) {
+  return array1.map((element, index) => {
+    return {
+      name: element,
+      average: array2[index].reduce((acc, curr, index2) => acc += curr, 0) / array2[index].length,
+    };
+  });
+};
+
+console.log(studentAverage(students, grades));
