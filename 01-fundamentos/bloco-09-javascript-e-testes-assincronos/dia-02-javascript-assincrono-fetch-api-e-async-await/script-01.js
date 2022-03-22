b575385f46b83dc435dd41ba92dc99452a22839e
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-22 14:53:04 -0300

    Adicionando comentários ao primeiro exercício

--- 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/script-01.js
+++ 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/script-01.js
@@ -5,13 +5,13 @@ const fetchJoke = () => {
   const myObject = {
     method: 'GET',
     headers: { 'Accept': 'application/json' }
-  };
+  }; // objeto contendo as informações sobre requisição de API (presente na documentação)
 
   // Resolução baseada no gabarito da trybe presente no course
   // source: https://app.betrybe.com/course/fundamentals/javascript-e-testes-assincronos/javascript-assincrono-fetch-api-e-asyncawait-gabarito/solutions/9fcb5438-1f4a-4820-93e6-b7627784dd2e/conteudo/06007a39-c333-4ed6-8918-841c44c5df00?use_case=calendar
   fetch(API_URL, myObject)
-    .then(response => response.json())
-    .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
+    .then(response => response.json()) // converte o conteúdo, retornando um JSON contendo as informações da piada
+    .then(data => document.getElementById('jokeContainer').innerHTML = data.joke); // then capaz de manipular dos dados recebidos no them acima, neste caso atriu o data.joke no h2 da página web criada
 };
 
 window.onload = () => fetchJoke();
