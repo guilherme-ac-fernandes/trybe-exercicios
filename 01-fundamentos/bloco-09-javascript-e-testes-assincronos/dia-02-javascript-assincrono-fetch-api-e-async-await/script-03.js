45611da7e5cecb046ee483b3bbd07d6d9423189d
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-22 16:10:28 -0300

    Incluindo referência no último exercício

--- 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/script-03.js
+++ 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/script-03.js
@@ -9,7 +9,8 @@ const fetchCoins = async () => {
   return coins;
 }
 
-
+// Resolução baseada no gabarito do course da Trybe (utilização de await em variáveis para retornar as informações obtidas anteriormente; forma de manipular a url da segunda API referente a currency e sua devida manipulação)
+// source: https://app.betrybe.com/course/fundamentals/javascript-e-testes-assincronos/javascript-assincrono-fetch-api-e-asyncawait-gabarito/solutions/9fcb5438-1f4a-4820-93e6-b7627784dd2e/conteudo/06007a39-c333-4ed6-8918-841c44c5df00?use_case=calendar
 const fetchCurrency = async () => {
   const baseUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest'
   const usdEndpoint = '/currencies/usd.min.json'
@@ -27,6 +28,7 @@ const setCoins = async () => {
   const coins = await fetchCoins();
   const currency = await fetchCurrency(); // currency.brl refere a dollar para real
 
+  // Resolução baseada no exercício anterior 
   const list = document.getElementById('coinContainer');
   coins.forEach((element, index) => {
     if (index < 10) {
