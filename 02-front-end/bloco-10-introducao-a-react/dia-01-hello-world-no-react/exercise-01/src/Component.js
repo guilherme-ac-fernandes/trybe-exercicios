0eefa40f032bb55450c165c9f07489db13b8e662
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-04-04 19:23:40 -0300

    Estilização do primeiro exercício

--- 02-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercise-01/src/Component.js
+++ 02-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercise-01/src/Component.js
@@ -4,9 +4,9 @@ export default function Component () {
   const name = 'Guilherme Fernandes';
   const description = 'PotterHead, amante de Nintendo e futuro Desenvolvedor Web/Engenheiro';
   return (
-    <div>
+    <main className='main'>
       <h1>{name}</h1>
       <p>{description}</p>
-    </div>
+    </main>
   )
 }
