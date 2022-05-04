cf58e2c3dddb6696d66e165f8d611789d3186661
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-05-04 15:52:34 -0300

    Adiciona arquivos para exercício 1 - req 3

---
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/exercise-01/requisito-03/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import InputTodo from './InputTodo';
+import Item from './Item';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listTodo: [],
+      selectedTodo: null,
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.selectTodo = this.selectTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  addTodo(todo) {
+    this.setState((state) => ({ listTodo: [...state.listTodo, todo] }));
+  }
+
+  selectTodo(indexTodo) {
+  }
+
+  removeTodo(indexTodo) {
+  }
+
+  render() {
+    const { listTodo } = this.state;
+
+    return (
+      <main className="App">
+        <InputTodo
+          addTodo={ (todo) => this.addTodo(todo) }
+        />
+        <ul>
+          { listTodo.map((todo, index) => (
+            <li key={ index + 1 }>
+
+              <Item content={ todo } />
+              {/* Colocar os botões aqui */}
+            </li>
+          )) }
+        </ul>
+      </main>
+    );
+  }
+}
+
+export default App;
