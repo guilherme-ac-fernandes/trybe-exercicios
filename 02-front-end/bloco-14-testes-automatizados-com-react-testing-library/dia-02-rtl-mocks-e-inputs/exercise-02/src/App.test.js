146c89f52e751c13b1e8b64a4e726bc819f43d02
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-05-05 19:00:40 -0300

    Adiciona arquivos do segundo exercício

---
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercise-02/src/App.test.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+describe('Teste da aplicação toda', () => {
+  it('renders App', async () => {
+
+  });
+});
