c4f9fd41f196c053d68705be7889d666ce9da733
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-05-05 15:37:56 -0300

    Exercício 1 resolvido

--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercise-01/src/service.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercise-01/src/service.js
@@ -1,4 +1,25 @@
-// Função para gerar número aleatório entre o 0 a 100 proveniente do CodeGreeper (link: https://www.codegrepper.com/code-examples/javascript/js+random+number+between+0+and+100)
+// Função para gerar número aleatório entre o 0 a 100 
+// Proveniente do CodeGreeper (link: https://www.codegrepper.com/code-examples/javascript/js+random+number+between+0+and+100)
 const randomNumber = () => Math.floor(Math.random() * 101);
 
-module.exports = { randomNumber };
+
+// Função que recebe string e retorna em caixa alta
+const uppercase = (string) => string.toUpperCase();
+
+// Função que recebe string e retorna a primeira letra
+const firstLetter = (string) => string[0];
+
+// Função que concatena duas strings
+const concatenate = (string1, string2) => string1 + string2;
+
+// Função que realiza uma requisição em uma API
+// Resolução proveniente do gabarito presente no course da Trybe (link: https://app.betrybe.com/course/front-end/testes-automatizados-com-react-testing-library/rtl-mocks-e-inputs/solutions/96709d24-8962-42f8-9318-a5dac56aacc5/exercicios/9cd35e26-f4a5-43cb-8b6e-70ab19fd0c6c?use_case=calendar)
+const fetchAPI = async () => {
+  return fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json()
+    .then(json => response.ok ? Promise.resolve(json) : Promise.reject(json)
+    )
+  );
+}
+
+module.exports = { randomNumber, uppercase, firstLetter, concatenate, fetchAPI };
--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercise-01/src/service.test.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercise-01/src/service.test.js
@@ -68,3 +68,105 @@ describe('Teste de novas implementações em cima da função randomNumber com m
     expect(service.randomNumber).toHaveBeenLastCalledWith(14);
   });
 });
+
+// Requisito 4
+describe('Testes de implementações nas funções uppercase, firstLetter e concatenate', () => {
+  it('verifica a implementação de nova funcionalidade a função uppercase', () => {
+    // Implementação da funcionalidade 
+    service.uppercase = jest.fn()
+      .mockImplementation((string) => string.toLowerCase());
+    
+    service.uppercase('PALAVRA');
+    expect(service.uppercase).toHaveBeenCalled();
+    expect(service.uppercase).toHaveBeenCalledTimes(1);
+    expect(service.uppercase('PALAVRA')).toBe('palavra');
+    expect(service.uppercase).toHaveBeenCalledTimes(2);
+    expect(service.uppercase).toHaveBeenLastCalledWith('PALAVRA');
+
+    // Reset da funcionalidade
+    service.uppercase.mockReset();
+    expect(service.uppercase('PALAVRA')).toBe(undefined);
+    expect(service.uppercase).toHaveBeenCalledTimes(1);
+    expect(service.uppercase).toHaveBeenLastCalledWith('PALAVRA');
+
+    // service.uppercase.mockRestore();
+    // expect(service.uppercase('palavra')).toBe('PALAVRA');
+  });
+
+  it('verifica a implementação de nova funcionalidade a função uppercase com jest.spyOn', () => {
+    // Implementação da funcionalidade 
+    const mockUpperCase = jest.spyOn(service, 'uppercase')
+      .mockImplementationOnce((string) => string.toLowerCase());
+    
+    expect(mockUpperCase('PALAVRA')).toBe('palavra');
+    expect(mockUpperCase).toHaveBeenCalled();
+    expect(mockUpperCase).toHaveBeenCalledTimes(1);
+    expect(mockUpperCase).toHaveBeenLastCalledWith('PALAVRA');
+
+    expect(mockUpperCase('PALAVRA')).toBe(undefined);
+    expect(mockUpperCase).toHaveBeenCalledTimes(2);
+    expect(mockUpperCase).toHaveBeenLastCalledWith('PALAVRA');
+  });
+
+  it('verifica a implementação de nova funcionalidade a função firstLetter', () => {
+    // Implementação da funcionalidade 
+    service.firstLetter = jest.fn()
+      .mockImplementation((string) => string[string.length - 1]);
+    
+    service.firstLetter('última');
+    expect(service.firstLetter).toHaveBeenCalled();
+    expect(service.firstLetter).toHaveBeenCalledTimes(1);
+    expect(service.firstLetter('última')).toBe('a');
+    expect(service.firstLetter).toHaveBeenCalledTimes(2);
+    expect(service.firstLetter).toHaveBeenLastCalledWith('última');
+
+    // Reset da funcionalidade
+    service.firstLetter.mockReset();
+    expect(service.firstLetter('última')).toBe(undefined);
+    expect(service.firstLetter).toHaveBeenCalledTimes(1);
+    expect(service.firstLetter).toHaveBeenLastCalledWith('última');
+  });
+
+  it('verifica a implementação de nova funcionalidade a função concatenate', () => {
+    // Implementação da funcionalidade 
+    service.concatenate = jest.fn()
+      .mockImplementation((string1, string2, string3) => string1 + string2 + string3);
+    
+    service.concatenate('a', 'b', 'c');
+    expect(service.concatenate).toHaveBeenCalled();
+    expect(service.concatenate).toHaveBeenCalledTimes(1);
+    expect(service.concatenate('a', 'b', 'c')).toBe('abc');
+    expect(service.concatenate).toHaveBeenCalledTimes(2);
+    expect(service.concatenate).toHaveBeenLastCalledWith('a', 'b', 'c');
+
+    // Reset da funcionalidade
+    service.concatenate.mockReset();
+    expect(service.concatenate('a', 'b', 'c')).toBe(undefined);
+    expect(service.concatenate).toHaveBeenCalledTimes(1);
+    expect(service.concatenate).toHaveBeenLastCalledWith('a', 'b', 'c');
+  });
+});
+
+// Requisito 5
+// Resolução proveniente do gabarito presente no course da Trybe (link: https://app.betrybe.com/course/front-end/testes-automatizados-com-react-testing-library/rtl-mocks-e-inputs/solutions/96709d24-8962-42f8-9318-a5dac56aacc5/exercicios/9cd35e26-f4a5-43cb-8b6e-70ab19fd0c6c?use_case=calendar)
+describe('Testes que verifica o retorno de uma API', () => {
+  service.fetchAPI = jest.fn();
+  afterEach(service.fetchAPI.mockReset)
+  
+  it('verifica se o retorno da API foi bem sucessida', async () => {
+    service.fetchAPI.mockReturnValue('request sucess');
+    
+    service.fetchAPI();
+    expect(service.fetchAPI).toHaveBeenCalled();
+    expect(service.fetchAPI).toHaveBeenCalledTimes(1);
+    await expect(service.fetchAPI()).resolves.toBe('request sucess');
+    expect(service.fetchAPI).toHaveBeenCalledTimes(2);
+  });
+
+  it('verifica se o retorno da API apresentou falha', async () => {
+    service.fetchAPI.mockReturnValue('request sucess');
+    expect(service.fetchAPI).toHaveBeenCalledTimes(0);
+    await expect(service.fetchAPI()).rejects.toBe('request sucess');
+    expect(service.fetchAPI).toHaveBeenCalledTimes(1);
+  });
+});
