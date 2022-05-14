972b9ef653bc8fe2b918819afac203ece3bbb84d
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-05-13 22:11:03 -0300

    ResoluáCão do segundo exercício

--- 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercise-02/src/pages/RegisteredCustomers.jsx
+++ 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercise-02/src/pages/RegisteredCustomers.jsx
@@ -2,29 +2,65 @@ import React from 'react';
 import LoginFailed from './LoginFailed';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Button from '../components/Button';
+import { removeAction, updateSortedAction } from '../redux/actions/actions';
 
 class RegisteredCustomers extends React.Component {
+  handleSortedByName = () => {
+    const { updateSorted } = this.props;
+    updateSorted();
+
+  }
+
+  removeCustomer = (customer) => {
+    const { removeObject } = this.props;
+    removeObject(customer);
+  }
+
+  displayCustomers = () => {
+    const { allCustumers, allCustumersSorted, sorted } = this.props;
+    if(sorted) return allCustumersSorted
+    return allCustumers
+  }
+
   render() {
-    const { loginInfo, allCustumers } = this.props;
+    const { loginInfo } = this.props;
+    const customersToDisplay = this.displayCustomers();
+    
     if (!('email' in loginInfo)) {
       return <LoginFailed />
     }
     return (
       <>
         <h1>Clientes cadastrados</h1>
-        <p>{loginInfo.email}</p>
-        <p>{loginInfo.password}</p>
+        <p>{`Olá ${loginInfo.email}`}</p>
+        <Link to="/register">Cadastro</Link>
+        <Button
+          type="button"
+          label="Ordem Alfabética"
+          onClick={ this.handleSortedByName }
+        />
         
         <section>
-          {allCustumers.length === 0 ? (
+          {customersToDisplay.length === 0 ? (
             <p>Nenhum cliente cadastrado</p>
           ) : (
             <ul>
-            {allCustumers.map((customer, index) => 
-              <li key={ index }>{customer.name}</li>)}
+            {customersToDisplay.map((customer, index) => 
+              <li key={ index }>
+                <p>{`Cadastro número ${ index + 1 }`}</p>
+                <p>{`Cliente: ${customer.name}`}</p>
+                <p>{`Idade: ${customer.age}`}</p>
+                <p>{`email: ${customer.email}`}</p>
+                <Button 
+                  type="button" 
+                  label="X" 
+                  onClick={ () => this.removeCustomer(customer) }
+                />
+              </li>
+            )}
             </ul>
-          )}
-          <Link to="/register">Cadastro</Link>
+          )} 
         </section>
       </>
     );
@@ -34,6 +70,13 @@ class RegisteredCustomers extends React.Component {
 const mapStateToProps = (state) => ({
   loginInfo: state.reducer.loginInfo,
   allCustumers: state.reducer.allCustumers,
+  allCustumersSorted: state.reducer.allCustumersSorted,
+  sorted: state.reducer.sorted,
 });
 
-export default connect(mapStateToProps)(RegisteredCustomers);
+const mapDispatchToProps = (dispatch) => ({
+  removeObject: (payload) => dispatch(removeAction(payload)),
+  updateSorted: () => dispatch(updateSortedAction()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegisteredCustomers);
--- 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercise-02/src/redux/reducers/reducer.js
+++ 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-03-usando-o-redux-no-react-pratica/exercise-02/src/redux/reducers/reducer.js
@@ -1,9 +1,18 @@
-import { MAKE_LOGIN } from "../actions/actions";
+import { MAKE_LOGIN, REGISTER, REMOVE_CUSTOMER, UPDATE_SORTED } from "../actions/actions";
 
 const INICIAL_STATE = {
   loginInfo: {},
   allCustumers: [],
+  allCustumersSorted: [],
+  sorted: false,
 };
+
+const sortByName = (a, b) => {
+  const nameA = a.name.toUpperCase(); 
+  const nameB = b.name.toUpperCase(); 
+  if (nameA < nameB) return -1;
+  if (nameA > nameB) return 1;
+}
 
 const reducer = (state = INICIAL_STATE, action) => {
   switch (action.type) {
@@ -12,8 +21,26 @@ const reducer = (state = INICIAL_STATE, action) => {
         ...state,
         loginInfo: { ...action.payload },
       };
-    default:
-      return state;
+    case REGISTER:
+      const newStateRegister = [ ...state.allCustumers, action.payload ];
+      return {
+        ...state,
+        allCustumers: [ ...state.allCustumers, action.payload ],
+        allCustumersSorted: newStateRegister.sort(sortByName),
+      };
+    case REMOVE_CUSTOMER:
+      const newStateRemove = state.allCustumers.filter((obj) => obj !== action.payload);
+      return {
+        ...state,
+        allCustumers: state.allCustumers.filter((obj) => obj !== action.payload),
+        allCustumersSorted: newStateRemove.sort(sortByName)
+      };
+      case UPDATE_SORTED:
+        return {
+          ...state,
+          sorted: !state.sorted,
+        };
+    default: return state;
   }
 };
 
