8493462d5f27a23dc5194083446c50405f5ccba2
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-06-03 01:12:07 -0300

    Remove arquivos de redux do exercício 3 do dia 17.1 e adiciona referência

--- 02-front-end/bloco-17-context-api-e-react-hooks/dia-01-context-api-do-react/exercise-03/src/App.js
+++ 02-front-end/bloco-17-context-api-e-react-hooks/dia-01-context-api-do-react/exercise-03/src/App.js
@@ -3,6 +3,8 @@ import Posts from './components/Posts';
 import Selector from './components/Selector';
 import RedditContext from './contextAPI/RedditContext';
 
+// Resolução proveniente do gabarito presente no course da Trybe
+// source: https://app.betrybe.com/course/front-end/context-api-e-react-hooks/context-api-do-react/solutions/17a3011e-be0e-4423-b8e1-015947b7fa47/gabarito-dos-exercicios/9a6b5643-a3fa-4cef-9706-ffefcf63bf2a?use_case=calendar
 class App extends Component {
   componentDidMount() {
     const { fetchPosts } = this.context;
