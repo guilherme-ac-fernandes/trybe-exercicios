08ca4e7cef455d728fbc6e8042fc0bafea037eb7
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-06-06 15:49:21 -0300

    Adiciona arquivos e resolução do segundo exercício com readme atualizado

---
+++ 02-front-end/bloco-17-context-api-e-react-hooks/dia-03-react-hooks-useeffect-e-hooks-customuzados/exercise-02/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Greeting from './Greeting';
+
+function App() {
+  return <Greeting />;
+}
+
+export default App;
---
+++ 02-front-end/bloco-17-context-api-e-react-hooks/dia-03-react-hooks-useeffect-e-hooks-customuzados/exercise-02/src/Greeting.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+function Greeting({ initialName = '' }) {
+  const storageName = window.localStorage.getItem('name') || initialName;
+  const [name, setName] = useState(storageName);
+
+  useEffect(() => {
+    window.localStorage.setItem('name', name);
+  }, [name]);
+
+  function handleChange({ target: { value } }) {
+    setName(value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={ (event) => event.preventDefault() }>
+        <label htmlFor="name">
+          Name:
+          <input onChange={ handleChange } id="name" />
+        </label>
+      </form>
+      { name ? <strong>{ `Hello ${name}` }</strong> : 'Please type your name' }
+    </div>
+  );
+}
+
+export default Greeting;
+
+Greeting.propTypes = {
+  initialName: PropTypes.string,
+};
+
+Greeting.defaultProps = {
+  initialName: '',
+};
