539d778ed57957fc2578396f55584ac4e0543424
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-07-08 14:30:21 -0300

    fix: resolução do exercício 5

--- 03-back-end/bloco-19-docker-utilizando-containers/dia-03-orquestrando-containers-com-docker-compose/exercise-05/src/App.js
+++ 03-back-end/bloco-19-docker-utilizando-containers/dia-03-orquestrando-containers-com-docker-compose/exercise-05/src/App.js
@@ -5,12 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Olá Tryber!!!.
         </p>
         <a
           className="App-link"
