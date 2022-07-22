dc339abd92a572e47d86aa36096912009209bb22
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-07-21 21:12:33 -0300

    feat: inseri dados no banco de dados zoo

--- 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-03-transformando-ideias-em-um-modelo-de-banco-de-dados/exercise-02/requisito-01.sql
+++ 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-03-transformando-ideias-em-um-modelo-de-banco-de-dados/exercise-02/requisito-01.sql
@@ -3,18 +3,30 @@ DROP DATABASE IF EXISTS zoo;
 CREATE DATABASE zoo;
 USE zoo;
 
+CREATE TABLE localizacao(
+  localizacao_id INT PRIMARY KEY AUTO_INCREMENT,
+  lugar VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE especie(
+  especie_id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE animais(
   animal_id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(50) UNIQUE NOT NULL,
-  especie VARCHAR(50) NOT NULL,
+  especie_id INT NOT NULL,
+  localizacao_id INT NOT NULL,
   sexo VARCHAR(50),
   idade INT,
-  localizacao VARCHAR(50) NOT NULL
+  FOREIGN KEY (especie_id) REFERENCES especie(especie_id),
+  FOREIGN KEY (localizacao_id) REFERENCES localizacao(localizacao_id)
 );
 
 CREATE TABLE gerente(
   gerente_id INT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(50) NOT NULL,
+  nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE cuidadores(
@@ -26,8 +38,42 @@ CREATE TABLE cuidadores(
 
 CREATE TABLE cuidadores_animais(
   id INT PRIMARY KEY AUTO_INCREMENT,
-  cuidador_id INT NOT NULL
+  cuidador_id INT NOT NULL,
   animal_id INT NOT NULL,
   FOREIGN KEY (cuidador_id) REFERENCES cuidadores(cuidador_id),
   FOREIGN KEY (animal_id) REFERENCES animais(animal_id)
 );
+
+INSERT INTO localizacao(lugar)
+VALUES('Recanto das Aves'),
+      ('Bosque dos Felinos'),
+      ('Savanna na Cidade'),
+      ('Floresta Encantada');
+
+INSERT INTO especie(nome)
+VALUES('Arara Azul'),
+      ('Onça'),
+      ('Leão'),
+      ('Elefante'),
+      ('Macaco');
+
+INSERT INTO animais(nome, especie_id, localizacao_id, sexo, idade)
+VALUES('Momo', 5, 4, 'Masculino', 3),
+      ('Juma', 2, 2, 'Feminino', 6),
+      ('Harry', 1, 1, 'Masculino', 1),
+      ('Emma', 4, 3, 'Feminino', 7);
+
+INSERT INTO gerente(nome)
+VALUES('Zelda'),
+      ('Link');
+
+INSERT INTO cuidadores(nome, gerente_id)
+VALUES('Peter', 1),
+      ('Jack', 2),
+      ('Ron', 1);
+
+INSERT INTO cuidadores_animais(cuidador_id, animal_id)
+VALUES(1, 1),
+      (2, 2),
+      (2, 3), 
+      (3, 4);
