fd2d9bdf66b9cf2b4cd1096e4c5c23f370a2d828
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-03 19:09:45 -0300

    fix: correção do docker-compose

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-06-nodejs-atividades-extras/exercise-01/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-06-nodejs-atividades-extras/exercise-01/index.js
@@ -4,12 +4,9 @@ const registerRoute = require('./registerRoute');
 const app = express();
 app.use(express.json());
 
+app.get('/', (_req, res) => res.status(200).json({ message: 'Tudo ok' }));
+
 app.use('/user', registerRoute);
-
-
-
-
-
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-06-nodejs-atividades-extras/exercise-01/middlewares/emailAndPassword.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-06-nodejs-atividades-extras/exercise-01/middlewares/emailAndPassword.js
@@ -1,13 +1,16 @@
 module.exports = (req, res, next) => {
   const { email, password } = req.body;
-
   if(!(email.match(/^[ae-z0-9.]+@[a-z0-9]+\.[a-z]+/i))) {
-    return res.status(400).json({ message: "email or password is incorrect" });
+    return res.status(400).json({
+      message: "email or password is incorrect",
+    });
   }
-
-  if(password.length < 4 || password.length > 8 || !password.match(/^[0-9]*$/)) {
-    return res.status(400).json({ message: "email or password is incorrect" });
+  if(password.length < 4
+    || password.length > 8
+    || !password.match(/^[0-9]*$/)) {
+    return res.status(400).json({
+      message: "email or password is incorrect",
+    });
   }
-  
   next();
 };
