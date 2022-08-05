c3ae1f9610e9e272c28debdf41304832789b184a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-05 15:44:12 -0300

    fix: formatação

--- 03-back-end/bloco-23-node.js-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/exercise-01/models/Author.js
+++ 03-back-end/bloco-23-node.js-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/exercise-01/models/Author.js
@@ -1,10 +1,10 @@
 const connection = require('./connection');
 
 const serialize = (authorData) => ({
-	id: authorData.id,
-	firstName: authorData.first_name,
-	middleName: authorData.middle_name,
-	lastName: authorData.last_name,
+  id: authorData.id,
+  firstName: authorData.first_name,
+  middleName: authorData.middle_name,
+  lastName: authorData.last_name,
 });
 
 const getNewAuthor = ({id, firstName, middleName, lastName}) => {
@@ -19,39 +19,35 @@ const getNewAuthor = ({id, firstName, middleName, lastName}) => {
 };
 
 const getAll = async () => {
-	const [authors] = await connection.execute(
-		'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
-	);
+  const [authors] = await connection.execute(
+    'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
+  );
   return authors.map(serialize).map(getNewAuthor);
 };
 
 const findById = async (id) => {
-	const query = 'SELECT first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
-	const [ authorData ] = await connection.execute(query, [id]);
-
-	if (authorData.length === 0) return null;
-	const data = authorData.map(serialize)[0];
-	return getNewAuthor({
-    ...data,
-		id,
-	});
+  const query = 'SELECT first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
+  const [ authorData ] = await connection.execute(query, [id]);
+  if (authorData.length === 0) return null;
+  const data = authorData.map(serialize)[0];
+  return getNewAuthor({ ...data, id });
 };
 
 const isValid = (firstName, middleName, lastName) => {
-	if (!firstName || typeof firstName !== 'string') return false;
-	if (!lastName || typeof lastName !== 'string') return false;
-	if (middleName && typeof middleName !== 'string') return false;
-	return true;
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (middleName && typeof middleName !== 'string') return false;
+  return true;
 };
 
 const create = async (firstName, middleName, lastName) => connection.execute(
-	'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?,?,?)',
-	[firstName, middleName, lastName],
+  'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?,?,?)',
+  [firstName, middleName, lastName],
 );
 
 module.exports = {
-	getAll,
+  getAll,
   findById,
-	isValid,
-	create,
+  isValid,
+  create,
 };
