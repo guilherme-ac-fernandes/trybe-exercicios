c4a3886bc90ec6ba7c8a911fb4d37ed88b3ca718
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-05 09:46:44 -0300

    feat: realização do primeiro exercício

---
+++ 03-back-end/bloco-23-node.js-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/exercise-01/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const Author = require('./models/Author');
+const Books = require('./models/Books');
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/authors', async (_req, res) => {
+	const authors = await Author.getAll();
+	res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+	const { id } = req.params;
+	const author = await Author.findById(id);
+	if (!author) return res.status(404).json({ message: 'Not found' });
+	res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+	const { first_name, middle_name, last_name } = req.body;
+	if (!Author.isValid(first_name, middle_name, last_name)) {
+		return res.status(400).json({ message: 'Dados inválidos' });
+	}
+	await Author.create(first_name, middle_name, last_name);
+	res.status(201).json({ message: 'Autor criado com sucesso! '});
+});
+
+app.get('/books', async (_req, res) => {
+  const allBooks = await Books.getAll();
+	res.status(200).json(allBooks);
+});
+
+app.get('/books/author/:id', async (req, res) => {
+  const { id } = req.params;
+  const allAuthorBooks = await Books.getByAuthorId(id);
+  res.status(200).json(allAuthorBooks);
+});
+
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+	const book = await Books.findById(id);
+	if (!book) return res.status(404).json({ message: 'Not found' });
+	res.status(200).json(book);
+});
+
+app.post('/books', async (req, res) => {
+	const { title, author_id } = req.body;
+	if (!await Books.isValid(title, author_id)) {
+		return res.status(400).json({ message: 'Dados inválidos' });
+	}
+	await Books.create(title, author_id);
+	res.status(201).json({ message: 'Autor criado com sucesso! '});
+});
+
+app.listen(PORT, () => {
+	console.log(`Ouvindo a porta ${PORT}`);
+});
