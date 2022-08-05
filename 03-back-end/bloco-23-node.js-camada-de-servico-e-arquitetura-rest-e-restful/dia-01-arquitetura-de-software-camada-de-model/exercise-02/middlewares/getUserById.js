329604db8b69b91acefb31345ecfdeb88f6ccd64
Guilherme Fernandes <97487898+guilherme-ac-fernandes@users.noreply.github.com>
2022-08-05 15:20:50 -0300

    Update getUserById.js

--- 03-back-end/bloco-23-node.js-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/exercise-02/middlewares/getUserById.js
+++ 03-back-end/bloco-23-node.js-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/exercise-02/middlewares/getUserById.js
@@ -2,8 +2,8 @@ const rescue = require('express-rescue');
 const UserModel = require('../models/User');
 
 module.exports = rescue(async (req, res) => {
-	const { id } = req.params;
-	const user = await UserModel.findById(id);
-	if (!user || user.length === 0) return res.status(404).json({message: "User not Found" });
-	return res.status(200).json(user);
+  const { id } = req.params;
+  const user = await UserModel.findById(id);
+  if (!user || user.length === 0) return res.status(404).json({message: "User not Found" });
+  return res.status(200).json(user);
 });
