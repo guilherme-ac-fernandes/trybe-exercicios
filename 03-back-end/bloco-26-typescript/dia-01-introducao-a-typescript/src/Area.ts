619ebc2bf22ad7cd0347152c8460b1c7ba672110
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-08 18:39:37 -0300

    feat: resolução do requisito 8

---
+++ 03-back-end/bloco-26-typescript/dia-01-introducao-a-typescript/src/Area.ts
@@ -0,0 +1,14 @@
+// Resolução baseada no gabarito presente no course da trybe
+// source: https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/94d0e996-1827-4fbc-bc24-c99fb592925b/section/4e3b7d3a-94a1-4fce-9545-0f2b04f8ccd9/day/f2bc13d9-91a6-488b-aa3f-257b0f5bb449/lesson/32b40011-0300-45ba-b73c-7ef1013c8411/solution
+
+const units = ["km²", "hm²", "dam²", "m²", "dm²", "cm²", "mm²"];
+
+function convert(value: number, fromUnit: string, toUnit: string) : number {
+  const fromIndex = units.indexOf(fromUnit);
+  const toIndex = units.indexOf(toUnit);
+  const exponent = (toIndex - fromIndex);
+
+  return value * Math.pow(100, exponent);
+}
+
+export default convert;
