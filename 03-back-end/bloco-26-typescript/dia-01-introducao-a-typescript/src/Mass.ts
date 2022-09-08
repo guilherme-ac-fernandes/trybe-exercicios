a41fea3af990c04da51c2fbd8d93ff271f01c1c3
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-08 18:51:33 -0300

    feat: resolução do requisito 10

--- 03-back-end/bloco-26-typescript/dia-01-introducao-a-typescript/src/Mass.ts
+++ 03-back-end/bloco-26-typescript/dia-01-introducao-a-typescript/src/Mass.ts
@@ -1,6 +1,7 @@
 // Resolução baseada no gabarito presente no course da trybe
 // source: https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/94d0e996-1827-4fbc-bc24-c99fb592925b/section/4e3b7d3a-94a1-4fce-9545-0f2b04f8ccd9/day/f2bc13d9-91a6-488b-aa3f-257b0f5bb449/lesson/32b40011-0300-45ba-b73c-7ef1013c8411/solution
 
+import readline from "readline-sync";
 const units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
 
 function convert(value: number, fromUnit: string, toUnit: string) : number {
@@ -11,4 +12,18 @@ function convert(value: number, fromUnit: string, toUnit: string) : number {
   return value * Math.pow(10, exponent);
 }
 
-export default convert;
+function exec(): void {
+  const value = readline.questionFloat("Digite o valor a ser convertido: \n");
+  const fromUnitChoiceIndex = readline.keyInSelect(units, "Escolha um número para a unidade base:");
+  const toUnitChoiceIndex = readline.keyInSelect(units, "Escolha um número para a conversão:");
+
+  const fromUnitChoice = units[fromUnitChoiceIndex];
+  const toUnitChoice = units[toUnitChoiceIndex];
+
+  const result = convert(value, fromUnitChoice, toUnitChoice);
+
+  const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}`;
+  console.log(message);
+}
+
+exec();
