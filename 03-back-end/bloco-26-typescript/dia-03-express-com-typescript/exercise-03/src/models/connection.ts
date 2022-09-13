8c7c86089ece3e6f445027fd62192afdf2c54603
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-13 01:48:40 -0300

    fix: correção do connection para utilziar o arquivo .env

--- 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/exercise-03/src/models/connection.ts
+++ 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/exercise-03/src/models/connection.ts
@@ -1,10 +1,12 @@
 import mysql from 'mysql2/promise';
 
+import 'dotenv/config';
+
 export default mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'docker',
-  database: 'TypeScriptExpress',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   // Referência: https://stackoverflow.com/questions/32100434/mysql-returns-full-datetime-string-on-select-query-when-column-type-is-date
   dateStrings: true,
 });
