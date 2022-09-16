aaac5cd88d0ac284f976fe5076a281ed16853e2f
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-16 15:32:42 -0300

    feat: resolução do quarto exercício

---
+++ 03-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-02-heranca-e-interfaces/src/Subject.ts
@@ -0,0 +1,16 @@
+export default class Subject {
+  constructor(
+    private _name: string,
+  ) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(newValue: string) {
+    if (newValue.length < 3) {
+      throw new Error('O nome deve conter no mínimo 3 caracteres.');
+    }
+    this._name = newValue;
+  }
+}
