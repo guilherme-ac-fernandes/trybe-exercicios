a95701299fb98bb0ef08ee43d354fe7f53c76b5d
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-16 15:29:58 -0300

    feat: adiciona comando e testa interface

--- 03-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-02-heranca-e-interfaces/src/Employee.ts
+++ 03-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-02-heranca-e-interfaces/src/Employee.ts
@@ -5,3 +5,16 @@ export default interface Employee {
 
   generateRegistration(): string;
 }
+
+const testInterfaceEmployee: Employee = {
+  registration: 'FNC1234567891011',
+  salary: 1200.00,
+  admissionDate: new Date(),
+
+  generateRegistration(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+    return `FNC${randomStr}`;
+  },
+};
+
+console.log(testInterfaceEmployee);
