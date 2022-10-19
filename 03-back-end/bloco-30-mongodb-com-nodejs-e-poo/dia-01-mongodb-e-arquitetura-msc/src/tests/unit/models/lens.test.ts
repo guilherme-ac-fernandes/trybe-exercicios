d3fc2d7d9e9c57f020f2e8911e817ac182d57dbb
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-19 12:37:02 -0300

    feat: resolução dos requisito 1 e 2

---
+++ 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/src/tests/unit/models/lens.test.ts
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import LensModel from '../../../models/Lens';
+import { Model } from 'mongoose';
+import { lensMock, lensMockWithId } from '../../mocks/lensMock';
+
+describe('Lens Model', () => {
+  const lensModel = new LensModel();
+
+	before(() => {
+		sinon.stub(Model, 'create').resolves(lensMockWithId);
+		sinon.stub(Model, 'findOne').resolves(lensMockWithId);
+	});
+
+	after(() => {
+		sinon.restore();
+	});
+
+  describe('creating a lens', () => {
+    it('successfully created', async () => {
+      const newLens = await lensModel.create(lensMock);
+      expect(newLens).to.be.deep.equal(lensMockWithId);
+    });
+  });
+    
+  describe('searching a frame', () => {
+    it('successfully found', async () => {
+    const lensFound = await lensModel.readOne('62cf1fc6498565d94eba52cd');  
+    expect(lensFound).to.be.deep.equal(lensMockWithId);
+    });
+
+    it('_id not found', async () => {
+      try {
+        await lensModel.readOne('123ERRADO');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
+
+});
