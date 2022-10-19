871f722f64609ef71d375056a80a5bb8d7bc7308
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-19 14:07:58 -0300

    refactor: alteração da localização dos arquivos do exercício 30.1

--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc-camada-model/src/interfaces/IModel.ts
+++
@@ -1,8 +0,0 @@
-interface IModel<T> {
-  create(obj:T):Promise<T>,
-  readOne(_id:string):Promise<T | null>,
-  read():Promise<T[] | null>,
-  destroy(_id:string):Promise<T | null>,
-}
-
-export default IModel;
--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc-camada-model/src/models/Connection.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import mongoose from 'mongoose';
-
-const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || 'mongodb://root:example@localhost:27017/glassesStore?authSource=admin',
-) => mongoose.connect(mongoDatabaseURI);
-
-export default connectToDatabase;
