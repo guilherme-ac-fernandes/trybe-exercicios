95d65744ea328d54dd38667579ff27483efbadc1
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-19 12:51:56 -0300

    feat: resolução do requisito 6 - criação do destroy no model genérico

--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/src/interfaces/IModel.ts
+++ 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/src/interfaces/IModel.ts
@@ -2,6 +2,7 @@ interface IModel<T> {
   create(obj:T):Promise<T>,
   readOne(_id:string):Promise<T | null>,
   read():Promise<T[] | null>,
+  destroy(_id:string):Promise<T | null>,
 }
 
 export default IModel;
--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/src/models/MongoModel.ts
+++ 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/src/models/MongoModel.ts
@@ -8,17 +8,22 @@ abstract class MongoModel<T> implements IModel<T> {
     this._model = model;
   }
 
-  public async create(obj:T):Promise<T> {
+  public async create(obj: T):Promise<T> {
     return this._model.create({ ...obj });
   }
     
-  public async readOne(_id:string):Promise<T | null> {
+  public async readOne(_id: string):Promise<T | null> {
     if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
     return this._model.findOne({ _id });
   }
 
   public async read():Promise<T[] | null> {
     return this._model.find();
+  }
+
+  public async destroy(_id: string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findByIdAndDelete({ _id });
   }
 }
 
