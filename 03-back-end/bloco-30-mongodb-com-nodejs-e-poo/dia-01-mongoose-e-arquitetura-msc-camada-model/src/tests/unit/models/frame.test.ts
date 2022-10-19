b485b0eab172aaf32ff85ff6ec7d264d6a797502
Guilherme Fernandes <97487898+guilherme-ac-fernandes@users.noreply.github.com>
2022-10-19 18:19:08 -0300

    Update frame.test.ts

--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongoose-e-arquitetura-msc-camada-model/src/tests/unit/models/frame.test.ts
+++ 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongoose-e-arquitetura-msc-camada-model/src/tests/unit/models/frame.test.ts
@@ -12,6 +12,7 @@ describe('Frame Model', () => {
 		sinon.stub(Model, 'findOne').resolves(frameMockWithId);
     sinon.stub(Model, 'find').resolves([frameMockWithId]);
     sinon.stub(Model, 'findByIdAndDelete').resolves(frameMockWithId);
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(frameMockWithId);
 	});
 
 	after(() => {
@@ -61,4 +62,20 @@ describe('Frame Model', () => {
       }
     });
   });
+
+  describe('updating a frame', () => {
+    it('successfully update', async () => {
+      const frameUpdate = await frameModel.update('62cf1fc6498565d94eba52cd', frameMock);
+      expect(frameUpdate).to.be.deep.equal(frameMockWithId);
+    });
+
+    it('_id not found', async () => {
+      try {
+        await frameModel.update('123ERRADO', frameMock);
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
 });
+
