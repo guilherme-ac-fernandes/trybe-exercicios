bc388c7552e878f84bbf82754f9d348ee9c732a9
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-29 23:37:33 -0300

    feat: adiciona uma segunda resolução em alguns exercícios do dia 0

--- 04-ciencia-da-computacao/bloco-33-introducao-a-python/dia-00-introducao-a-cs/exercises/repetition.py
+++ 04-ciencia-da-computacao/bloco-33-introducao-a-python/dia-00-introducao-a-cs/exercises/repetition.py
@@ -7,12 +7,26 @@ def append_each_letter_of_the_word_in_a_list(word: str):
 
 
 def return_index_of_the_uppercase_letter(word: str):
-    upper_char = [index for index, letter in enumerate(word) if letter.isupper()]
+    # Resolução fornecida pela equipe de instrução da Trybe
+    # source: https://github.com/tryber/sd-020-b-live-lectures/
+    # blob/CS-1.0-Mentoria-introducaoPython/exercises/repetition.py
+    # for letra in word:
+    #     if letra.isupper() == True:
+    #         return word.index(letra)
+    upper_char = [index
+                  for index, letter in enumerate(word)
+                  if letter.isupper()]
     return upper_char[0]
 
- 
+
 def return_element_from_list_that_is_string(input_list):
-    string = [string 
-            for string in input_list 
-            if isinstance(string, str)]
+    # Resolução fornecida pela equipe de instrução da Trybe
+    # source: https://github.com/tryber/sd-020-b-live-lectures/
+    # blob/CS-1.0-Mentoria-introducaoPython/exercises/repetition.py
+    # for ele in input_list:
+    #     if isinstance(ele, str):
+    #         return ele
+    string = [string
+              for string in input_list 
+              if isinstance(string, str)]
     return string[0]
