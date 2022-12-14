0f0a94e7c3d6acc8a497690d31f3b23b86a04216
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-14 16:12:37 -0300

    feat: resolução do primeiro exercício

---
+++ 04-ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-03-padroes-decorator-observer-factory/src/color_log.py
@@ -0,0 +1,44 @@
+ROSA = "\033[95m"
+ROXO = "\033[94m"
+AZUL = "\033[96m"
+VERDE = "\033[92m"
+LARANJA = "\033[93m"
+VERMELHO = "\033[91m"
+NEGRITO = "\033[1m"
+SUBLINHADO = "\033[4m"
+RESET = "\033[0m"
+
+
+class Log:
+    def dispara_log(message):
+        return message
+
+
+class LogError(Log):
+    def __init__(self, log):
+        self.log = log
+
+    def dispara_log(self, message):
+        return f"{VERMELHO}{self.log.dispara_log(message)}{RESET}"
+
+
+class LogWarning(Log):
+    def __init__(self, log):
+        self.log = log
+
+    def dispara_log(self, message):
+        return f"{LARANJA}{self.log.dispara_log(message)}{RESET}"
+
+
+class LogSuccess(Log):
+    def __init__(self, log):
+        self.log = log
+
+    def dispara_log(self, message):
+        return f"{VERDE}{self.log.dispara_log(message)}{RESET}"
+
+
+if __name__ == "__main__":
+    print(LogError(Log).dispara_log('O sistema está com erros'))
+    print(LogWarning(Log).dispara_log('O sistema está lento'))
+    print(LogSuccess(Log).dispara_log('O sistema está funcionando'))
