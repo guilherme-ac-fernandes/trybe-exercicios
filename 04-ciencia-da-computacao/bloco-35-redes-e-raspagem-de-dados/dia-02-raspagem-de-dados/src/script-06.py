0d95db65c64485be31f759675ba19f8b55910afe
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-13 19:09:35 -0300

    feat: adiciona commandos para criação do banco com mongoimport

--- 04-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/src/script-06.py
+++ 04-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/src/script-06.py
@@ -1,5 +1,11 @@
 from pymongo import MongoClient
 
+# Criação do banco de dados com mongoimport
+# docker cp books.json mongodb:/tmp/books.json
+# cd tmp
+# mongoimport --db library --collection books (continua)
+#  --drop --file books.json --jsonArray
+
 
 category = input("Escolha uma categoria: ")
 with MongoClient() as client:
--- 04-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/src/script-07.py
+++ 04-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/src/script-07.py
@@ -1,6 +1,13 @@
 from pymongo import MongoClient
 
 
+# Criação do banco de dados com mongoimport
+# docker cp books.json mongodb:/tmp/books.json
+# cd tmp
+# mongoimport --db library --collection books (continua)
+#  --drop --file books.json --jsonArray
+
+
 with MongoClient() as client:
     db = client.library
     pipelines = [
