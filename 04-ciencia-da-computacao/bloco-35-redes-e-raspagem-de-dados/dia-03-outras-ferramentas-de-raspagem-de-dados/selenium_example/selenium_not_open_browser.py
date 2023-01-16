e373e0e82f15c23d39fdc638d3ad31126bcee04a
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-16 15:46:22 -0300

    feat: adiciona exemplos das ferramentas

---
+++ 04-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-03-outras-ferramentas-de-raspagem-de-dados/selenium_example/selenium_not_open_browser.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+# Importa a classe 'Options' do browser
+from selenium.webdriver.firefox.options import Options
+
+
+firefox = webdriver.Firefox()
+
+# Instancia um objeto da classe 'Options'
+options = Options()
+# Adiciona um argumento passando o parâmetro '--headless'
+options.add_argument('--headless')
+
+# Define que a instância do navegador deve usar as options definidas
+firefox = webdriver.Firefox(options=options)
+
+# firefox.get('https://books.toscrape.com/')
+
+# ...
