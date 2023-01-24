9145c6e817e1d44c7c386795dfabb35225e920eb
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-24 14:03:33 -0300

    feat: adiciona exemplos de funções de ordenação e busca

---
+++ 04-ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/examples/bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(numbers):
+    n = len(numbers)
+
+    for ordered_elements in range(n - 1):
+        for item in range(0, n - 1 - ordered_elements):
+            if numbers[item] > numbers[item + 1]:
+                # current_element = numbers[item]
+                # numbers[item] = numbers[item + 1]
+                # numbers[item + 1] = current_element
+                index = item + 1
+                numbers[item], numbers[index] = numbers[index], numbers[item]
+    return numbers
+
+
+numbers = [7, 5, 9, 2, 6, 8]
+print(bubble_sort(numbers))
---
+++ 04-ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/examples/merge_sort.py
@@ -0,0 +1,51 @@
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    # se não reduzi o suficiente, continua
+    if (end - start) > 1:
+        # encontrando o meio
+        mid = (start + end) // 2
+        # dividindo as listas
+        merge_sort(numbers, start, mid)
+        merge_sort(numbers, mid, end)
+        # unindo as listas
+        merge(numbers, start, mid, end)
+
+# função auxiliar que realiza a mistura dos dois arrays
+
+
+def merge(numbers, start, mid, end):
+    # indexando a lista da esquerda
+    left = numbers[start:mid]
+    # indexando a lista da direita
+    right = numbers[mid:end]
+
+    # as duas listas começarão do início
+    left_index, right_index = 0, 0
+
+    # percorrer sobre a lista inteira como se fosse uma
+    for general_index in range(start, end):
+        # se os elementos da esquerda acabaram, preenche o restante
+        # com a lista da direita
+        if left_index >= len(left):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        # se os elementos da direita acabaram, preenche o restante
+        # com a lista da esquerda
+        elif right_index >= len(right):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        # se o elemento do topo da esquerda for menor que o da direita,
+        # ele será o escolhido
+        elif left[left_index] < right[right_index]:
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            # caso o da direita seja menor, ele será o escolhido
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
+numbers = [6, 5, 3, 1, 8, 7, 2, 4]
+merge_sort(numbers, 0, len(numbers))
+print(numbers)
